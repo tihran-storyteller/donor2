88caea05c84f226e3590d5fabb5556133c92a0bf
tihran <tihran.chilinharian@getstoryteller.com>
2024-12-27 11:41:50 +0100

    m

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "donor2"
+include(":app")
+include(":showcase")
---
+++ showcase/src/main/java/com/example/showcase/ShocaseModuleClaas.kt
@@ -0,0 +1,10 @@
+package com.example.showcase
+
+import android.util.Log
+
+class ShocaseModuleClaas {
+
+    fun testShowcaseFuns(){
+        Log.d("wtf", "dwt")
+    }
+}
